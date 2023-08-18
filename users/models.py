5f16beab27955293d32d2efb374fc1d1cd241f1d
jhhyun <jhh980622@naver.com>
2023-08-18 19:26:46 +0900

    nothing changed

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     kakao_id = models.CharField(max_length=100, blank=True, null=True)
     kakao_token = models.CharField(max_length=200, blank=True, null=True)
+    is_user = models.BooleanField(default= False)
 
     def __str__(self):
         return self.username
